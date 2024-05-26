563d1d748d8a288d3e7eaaddcfc6e52937b59b8a
Ivan <114814073+logvin4uk@users.noreply.github.com>
2024-05-26 19:17:21 +0300

    update structure pj add my files

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Product from './Product';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
