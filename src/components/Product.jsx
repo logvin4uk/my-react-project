63e0b1c8ec9304a1891078d1bfde0009f1d80aae
Ivan <114814073+logvin4uk@users.noreply.github.com>
2024-05-26 19:46:58 +0300

    small changes

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,13 @@
-const Product = () => {
-  return (
-    <div>
-      <h2>Tacos</h2>
-      <p>Price: 999 credits</p>
-    </div>
-  );
-};
+// src/components/Product.jsx
 
-export default Product;
+export const Product = ({
+  name,
+  imgUrl = 'https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder',
+  price,
+}) => (
+  <div>
+    <h2>{name}</h2>
+    <img src={imgUrl} alt={name} width="480" />
+    <p>Price: {price} credits</p>
+  </div>
+);
